8712108d6ad1d41d4f921cadb6d99446a300f7e5
guoxuan.wang@transsion.com <61870782+HamsterCat-W@users.noreply.github.com>
2024-03-29 16:59:31 +0800

    build: 引入 less

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,11 @@
 <template>
-  <h1>This is an about page</h1>
+  <h1 class="title">This is an about page</h1>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped lang="less">
+.title {
+  color: var(--page-title-color);
+}
+</style>
