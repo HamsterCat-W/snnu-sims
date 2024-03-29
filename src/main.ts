d5abdeb54eb6206bc9c577e1539e164d083010f1
ovoCieray <1248725680@qq.com>
2024-03-29 09:49:57 +0800

    stu

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
 import './style.css'
 import App from './App.vue'
+
 import router from './router/index.ts'
 
 const app = createApp(App)
 const pinia = createPinia()
 
-app.use(router).use(pinia)
+app.use(router).use(pinia).use(ArcoVue)
 
 app.mount('#app')
